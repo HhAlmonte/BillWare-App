a331b06ee3e38744c07835dd98887629469604bf
HhAlmonte <20210299@itla.edu.do>
2023-08-14 09:42:58 -0400

    .

--- BillWare.App/Configuration.cs
+++ BillWare.App/Configuration.cs
@@ -3,6 +3,7 @@
     public static class Configuration
     {
         public const string APP_NAME = "BillWare";
+        /*public const string BASE_API_URL = "http://www.billwareapi.somee.com/api/";*/
         public const string BASE_API_URL = "https://localhost:7050/api/";
     }
 }
